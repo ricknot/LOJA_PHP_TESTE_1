62470eb547d43efc8eb566ac377c424d81b9526b
ricknot <87404660+ricknot@users.noreply.github.com>
2021-08-31 22:41:50 -0300

    Delete LOJA_PHP_TESTE directory

--- LOJA_PHP_TESTE/public/js/form_mask.js
+++
@@ -1,8 +0,0 @@
-         jQuery(function ($) {
-             $("input.Datanasc").mask("99/99/9999", {reverse: true});
-             $("input.TEL").mask("(99) 9999-9999", {reverse: true});
-             $("input.CEL").mask("(99) 9999?9-9999", {reverse: true});
-             $("input.CPF").mask("999.999.999-99", {reverse: true});
-             $("input.CEP").mask("99999-999", {reverse: true});
-             $("input.CNPJ").mask("99.999.999/9999-99", {reverse: true});
-         });
